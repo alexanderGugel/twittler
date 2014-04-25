7e8e8d921ac91a72a99ead36742d95cedee669e1
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-25 11:49:41 +0200

    Create displayTweets()-function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
+  var last = 0;
+  var stream = streams.home;
   
-  
-  for (var i = 0; i < streams.home.length; i++) {
-    var tweet = streams.home[i];
-    var $tweet = $('<article></article>');
-    var tweet ='<a href="#">@' + tweet.user + '</a>: ' + tweet.message;
-    var time = '<br><small>' + moment(tweet.created_at).fromNow() + '</small>';
-    $tweet.html(tweet + time);
-    $tweet.appendTo($body);
+  var changeStream = function (newStream) {
+    last = 0;
+    stream = newStream;
   }
+  
+  var displayTweets = function () {
+    for (var i = last; i < stream.length; i++) {
+      var tweet = streams.home[i];
+      var $tweet = $('<article></article>');
+      var msg ='<a data-user="' + tweet.user + '" href="#">@' + tweet.user + '</a>: ' + tweet.message;
+      var time = '<br><small>' + moment(tweet.created_at).fromNow() + '</small>';
+      $tweet.html(msg + time);
+      $tweet.prependTo($('.tweets'));
+    }
+    last = i;
+  }
+  
+  setInterval(function () {
+    displayTweets();
+  }, 60);
   
 });
