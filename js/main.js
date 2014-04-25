9faf47669eda501409d70ab564226590ad6a6053
Alexander Gugel <alexander.gugel@gmail.com>
2014-04-25 11:34:50 +0200

    Show date of tweets in human-friendly way

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ $(document).ready(function(){
   var $body = $('body');
   $body.html('');
   
+  
   for (var i = 0; i < streams.home.length; i++) {
     var tweet = streams.home[i];
     var $tweet = $('<article></article>');
-    $tweet.html('<a href="#">@' + tweet.user + '</a>: ' + tweet.message);
-    $tweet.appendTo($body); 
+    var tweet ='<a href="#">@' + tweet.user + '</a>: ' + tweet.message;
+    var time = '<br><small>' + moment(tweet.created_at).fromNow() + '</small>';
+    $tweet.html(tweet + time);
+    $tweet.appendTo($body);
   }
   
 });
